c905dcd73eeed6216b1fb5000851fe25342b2803
Kate Bennert <kate.bennert@gmail.com>
2022-10-10 09:51:10 -0400

    commit tree f8f0c9e77e11ec4be261aab3ed670d44a97923ee

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,9 +1,13 @@
 import React from "react";
 import PlantCard from "./PlantCard";
 
-function PlantList() {
+function PlantList({ plants, search }) {
+
+  const allPlants = plants.map(plant => <PlantCard key={plant.id} plant={plant} />);
+  const searchedPlants = allPlants.filter(plant => plant.props.plant.name.includes(search));
+
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
+    <ul className="cards">{(search === "") ? allPlants : searchedPlants}</ul>
   );
 }
 
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+
+  const [plants, setPlants] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then(r => r.json())
+      .then(plantData => setPlants(plantData))
+  }, []);
+
+  function onAddPlant(newPlantData) {
+    setPlants([...plants, newPlantData])
+  }
+
+  function onSearchChange(e) {
+    setSearch(e.target.value);
+  }
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onAddPlant={onAddPlant} />
+      <Search onSearchChange={onSearchChange}/>
+      <PlantList plants={plants} search={search}/>
     </main>
   );
 }
